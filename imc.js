9645c3600736e28dc46b080bf996208d43168cfa
igor <igor.rafael.briano@gmail.com>
2022-06-07 22:17:07 -0300

    exercicio 4 ok

--- imc.js
+++ imc.js
@@ -1,4 +1,12 @@
+const readlineSync = require('readline-sync');
 
-const CalculoIMC = (altura, peso) => peso/Math.pow(altura, 2);
+const CalculoIMC = (altura, peso) => peso/Math.pow((altura)/100, 2);
 
-console.log(CalculoIMC(1.71,73));
+const PedeInformacao = () => {
+  const altura = readlineSync.questionInt("Qual sua altura em centimetros?\n", {limitMessage: "Insira sua idade em cm ex: '170'"});
+  const peso = readlineSync.questionFloat("Qual seu peso ?\n");
+
+  return CalculoIMC(altura, peso)
+}
+
+console.log(PedeInformacao());
