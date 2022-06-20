27743f684886b1dd6aed8f89b6d150134586ca3e
igor rafael <igor.rafael.briano@gmail.com>
2022-06-20 18:54:50 -0300

    feat: requisito 5 ok

--- test2.js
+++ test2.js
@@ -1,8 +1,18 @@
 const { expect } = require('chai');
+const fs = require('fs').promises;
+const sinon = require('sinon');
 
 const writeFile = require('./writeFile');
 
 describe('testes para escrita em arquivo', () => {
+  before(() => {
+    sinon.stub(fs, 'writeFile');
+  });
+
+  after(() => {
+    fs.writeFile.restore();
+  });
+
   describe('verifica se é uma string', () => {
     it('retorna uma string', async () => {
       const resposta = await writeFile('./file0.txt', 'isso é um teste meu chegado');
--- writeFile.js
+++ writeFile.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 async function writeFile(arquivo, texto) {
   try {
     await fs.writeFile(arquivo, texto);
-    
+
     return 'ok';
   } catch (error) {
     console.log(error);
